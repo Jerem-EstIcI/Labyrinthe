3a8fcf058cae77cfdeff73ae3a87a086c5f162e2
jerem <114385056+Jerem-EstIcI@users.noreply.github.com>
2024-04-12 19:10:45 +0200

    deplacement "création d'un lab" -> "main" + add ameliorer_lab

--- gen_lab.py
+++ gen_lab.py
@@ -14,9 +14,14 @@ class GenLab:
         - lab : grille comportant le labyrinthe non resolvable avec : 0 les chemins, 1 les murs, 2 l'entrée, 3 la sortie
         """
         lab = [[1 for ligne in range(self.dim)] for colonne in range(self.dim)]
+        colonne=0
+        ligne=0
         for i in range(len(lab)):
             for j in range(len(lab)):
-                lab[i][j] = randint(0, 1)
+                if i == 0 or j == 0 or j == self.dim-1 or i == self.dim-1:
+                    lab[i][j]=1
+                else:
+                    lab[i][j] = randint(0, 1)
         lab[0][0] = 2  # début
         lab[self.dim - 1][self.dim - 1] = 3  # fin
         return lab
@@ -35,22 +40,51 @@ class GenLab:
         j = 0
         start = time.time()
         while chemin != arrive:
-            if i + 1 < self.dim and self.tab[i + 1][j] == 0:
-                i += 1
-            elif j + 1 < self.dim and self.tab[i][j + 1] == 0:
+            if i + 1 < self.dim and self.tab[i + 1][j] == 0:    # si la prochaine case  horizontale est un chemin alors on avance a la prochaine case  
+                i += 1                                           
+            elif j + 1 < self.dim and self.tab[i][j + 1] == 0:  # sinon si la prochaine case verticale est un chemin alors on avance a la prochaine case  
                 j += 1
-            elif j + 1 < self.dim and self.tab[i][j + 1] == 1:
+            elif j + 1 < self.dim and self.tab[i][j + 1] == 1:  # sinon si la prochaine case verticale est un murs on le remplace par un chemin et on avance a la prochaine case  
                 self.tab[i][j + 1] = 0
                 j += 1
-            elif i + 1 < self.dim and self.tab[i + 1][j] == 1:
+            elif i + 1 < self.dim and self.tab[i + 1][j] == 1:  # sinon si la prochaine case horizontale est un murs on le remplace par un chemin et on avance a la prochaine case  
                 self.tab[i + 1][j] = 0
                 i += 1
-            elif i + 1 < self.dim and self.tab[i + 1][j] == 3:
+            elif i + 1 < self.dim and self.tab[i + 1][j] == 3:  # sinon si la prochaine case horizontale est l'arrivée alors on avance a la prochaine case  
                 i += 1
-            else:
+            else:                                               # sinon prochaine case verticale est l'arrivée alors on avance a la prochaine case
                 j += 1
             chemin = self.tab[i][j]
         end = time.time()
         print(self.tab, i, j)
         print("labyrinthe généré en", end - start, "secondes")
+        return self.tab
+    
+    def ameliorer_lab(self):
+        """
+        paramètre:
+        - dim : dimension du labyrinthe
+        - tab : grille comportant le labyrinthe resolvable ou non avec : 0 les chemins, 1 les murs, 2 l'entrée, 3 la sortie
+        renvoie:
+        - tab : grille comportant le labyrinthe sans les chemins et murs inutiles resolvable ou non avec : 0 les chemins, 1 les murs, 2 l'entrée, 3 la sortie
+        """
+        i=0
+        j=0
+        for i in range(len(self.tab)):
+            for j in range(len(self.tab[i])):
+
+                if i - 1 >= 0 and i + 1 < self.dim - 1 and j - 1 >= 0 and j + 1 < self.dim - 1: 
+                    # cas pour enlever les murs entouré des chemins
+                    if self.tab[i + 1][j] == 1 and self.tab[i - 1][j] == 1 and self.tab[i][j + 1] == 1 and self.tab[i][j - 1] == 1: 
+                        self.tab[i][j] = 0
+                    # cas pour enlever les murs entouré des chemins
+                    elif self.tab[i + 1][j] == 0 and self.tab[i - 1][j] == 0 and self.tab[i][j + 1] == 0 and self.tab[i][j - 1] == 0 and self.tab[i + 1][j + 1] == 0 and self.tab[i - 1][j + 1] == 0 and self.tab[i - 1][j - 1] == 0 and self.tab[i - 1][j - 1] == 0: 
+                        self.tab[i][j] = 0
+
+                    # cas pour mettre un mur si il est entouré par des chemins 
+                    elif self.tab[i + 1][j] == 0 and self.tab[i - 1][j] == 0 and self.tab[i][j + 1] == 0 and self.tab[i][j - 1] == 0 and self.tab[i + 1][j + 1] == 0 and self.tab[i - 1][j + 1] == 0 and self.tab[i - 1][j - 1] == 0 and self.tab[i - 1][j - 1] == 0:
+                        self.tab[i][j] = 1
+                    
+                    elif self.tab[]
+
         return self.tab
--- main.py
+++ main.py
@@ -1,8 +1,34 @@
+from tkinter import *
 from gen_lab import *
 
-gen = GenLab(25)
-gen.dessine_lab()
+gen = GenLab(25) # int : dimension du labyrinthe
+gen.ameliorer_lab()
+labyrinthe = gen.dessine_lab()
+
 
 #print(dir(gen))
 #help(gen.creer_lab)
 #help(gen.dessine_lab)
+
+
+
+# Création de la fenêtre tkinter
+app = Tk()
+app.title("Labyrinthe")
+
+# Création d'un canvas pour afficher le labyrinthe
+canvas = Canvas(app, width=len(labyrinthe[0]) * 20, height=len(labyrinthe) * 20)
+canvas.pack()
+
+# Parcours du labyrinthe pour dessiner les murs et les espaces
+for i in range(len(labyrinthe)):
+    for j in range(len(labyrinthe[i])):
+        if labyrinthe[i][j] == 1:
+            canvas.create_rectangle(j * 20, i * 20, (j + 1) * 20, (i + 1) * 20, fill="black")
+        else:
+            canvas.create_rectangle(j * 20, i * 20, (j + 1) * 20, (i + 1) * 20, fill="white")
+
+# Lancer la boucle principale de tkinter
+app.mainloop()
+
+"""def resolve avec backtracking """

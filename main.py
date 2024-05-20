03f30507e71a003c9f67d2e6b6520ab825e61709
jerem <114385056+Jerem-EstIcI@users.noreply.github.com>
2024-05-20 17:49:46 +0200

    résolution du labyrinthe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from gen_lab import GenLab
+import res_lab
 
 # Création de la fenêtre tkinter
 app = tk.Tk()
@@ -15,22 +16,25 @@ button_frame.pack(side=tk.TOP)
 canvas = tk.Canvas(app, width=0, height=0)  # Initialiser le canevas avec une taille arbitraire
 canvas.pack()
 
+labyrinthe_global = None
+dim_global = 101 # dimension du labyrinthe (max lisible 101)
+
 def affiche_lab():
+    global labyrinthe_global, dim_global
     '''
     Affiche le labyrinthe sur l'application tkinter
     '''
-    dim=25 # dimension du labyrinthe (max lisible 101)
-    dimcanva=500/dim
+    dimcanva=500/dim_global
 
-    gen = GenLab(dim) # GenLab(dimension)
-    labyrinthe = gen.dessine_lab()
+    gen = GenLab(dim_global) # GenLab(dimension)
+    labyrinthe_global = gen.dessine_lab()
 
 
-    canvas.config(width=len(labyrinthe[0]) * dimcanva, height=len(labyrinthe) * dimcanva)
+    canvas.config(width=len(labyrinthe_global[0]) * dimcanva, height=len(labyrinthe_global) * dimcanva)
     canvas.delete("all")  # Effacer tout ce qui est dessiné précédemment sur le canvas
 
     # Parcours du labyrinthe pour dessiner les murs et les espaces
-    for i, ligne in enumerate(labyrinthe):
+    for i, ligne in enumerate(labyrinthe_global):
         for j, case in enumerate(ligne):
             if case == 1:
                 canvas.create_rectangle(j * dimcanva, i * dimcanva, (j + 1) * dimcanva, (i + 1) * dimcanva, fill="black")
@@ -43,11 +47,34 @@ def affiche_lab():
 
 
 
+def resoudre_lab():
+    global labyrinthe_global, dim_global
+    # Récupérer le labyrinthe et la dimension depuis la fonction affiche_lab
+    lab_resolu = res_lab.res_lab(labyrinthe_global, dim_global)
+    dimcanva = 500 / dim_global
+    canvas.delete("all")  # Effacer tout ce qui est dessiné précédemment sur le canvas
+
+    # Parcours du labyrinthe pour dessiner les murs et les espaces
+    for i, ligne in enumerate(lab_resolu):
+        for j, case in enumerate(ligne):
+            if case == 1:
+                canvas.create_rectangle(j * dimcanva, i * dimcanva, (j + 1) * dimcanva, (i + 1) * dimcanva, fill="black")
+            elif case==2:
+                canvas.create_rectangle(j * dimcanva, i * dimcanva, (j + 1) * dimcanva, (i + 1) * dimcanva, fill="green")
+            elif case==3:
+                canvas.create_rectangle(j * dimcanva, i * dimcanva, (j + 1) * dimcanva, (i + 1) * dimcanva, fill="red")
+            elif case==5:
+                canvas.create_rectangle(j * dimcanva, i * dimcanva, (j + 1) * dimcanva, (i + 1) * dimcanva, fill="purple")
+            else:
+                canvas.create_rectangle(j * dimcanva, i * dimcanva, (j + 1) * dimcanva, (i + 1) * dimcanva, fill="white")
+
+
+
 
 gen_button = tk.Button(button_frame, text="Générer Labyrinthe", command=affiche_lab)
 gen_button.pack(side=tk.LEFT, padx=5, pady=5)
 
-res_button = tk.Button(button_frame, text="Résoudre Labyrinthe", command=affiche_lab)
+res_button = tk.Button(button_frame, text="Résoudre Labyrinthe", command=resoudre_lab)
 res_button.pack(side=tk.LEFT, padx=5, pady=5)
 
 # Lancer la boucle principale de tkinter
---
+++ res_lab.py
@@ -0,0 +1,51 @@
+def longueur_lab(lab, dim):
+    nbfois=0
+    while nbfois<=10:
+        for i in range(1, dim - 1):
+            for j in range(1, dim - 1):
+                if lab[i][j] == 2:
+                    lab[i][j] = 10
+                elif lab[i][j] == 0 or lab[i][j] == 3:
+                    if i > 0 and lab[i-1][j] >= 10:
+                        lab[i][j] = lab[i-1][j] + 1
+                    elif i < dim - 1 and lab[i+1][j] >= 10:
+                        lab[i][j] = lab[i+1][j] + 1
+                    elif j > 0 and lab[i][j-1] >= 10:
+                        lab[i][j] = lab[i][j-1] + 1
+                    elif j < dim - 1 and lab[i][j+1] >= 10:
+                        lab[i][j] = lab[i][j+1] + 1
+        nbfois+=1
+    print("labyrinthe avec longueur :",lab)
+    return lab
+
+def lab_forme(lab,dim):
+    for i in range(1, dim - 1):
+        for j in range(1, dim - 1):
+            if lab[i][j]>5:
+                lab[i][j]=0
+    return lab
+
+def res_lab(lab, dim):
+    reslab = longueur_lab(lab, dim)
+    posx = dim - 2
+    posy = dim - 2
+    if reslab[posx][posy]==3:
+        return 
+    while not (posx == 1 and posy == 1):
+        if reslab[posx-1][posy] == reslab[posx][posy] - 1:
+            reslab[posx][posy] = 5
+            posx -= 1
+        elif reslab[posx][posy-1] == reslab[posx][posy] - 1:
+            reslab[posx][posy] = 5
+            posy -= 1
+        elif reslab[posx+1][posy] == reslab[posx][posy] - 1:
+            reslab[posx][posy] = 5
+            posx += 1
+        elif reslab[posx][posy+1] == reslab[posx][posy] - 1:
+            reslab[posx][posy] = 5
+            posy += 1
+    reslab[1][1]=5
+    print("labyrinthe résolu :",reslab)
+    labyrinthe_resou=lab_forme(reslab,dim)
+    print("labyrinthe mit en forme :",labyrinthe_resou)
+    return labyrinthe_resou
